a08ef466b4621355a878eb5c347220409324616d
sigmoidanalytics <benedick.agdipa@gmail.com>
2024-11-27 18:04:52 +1100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 const withMT = require("@material-tailwind/react/utils/withMT");
 
-
 /** @type {import('tailwindcss').Config} */
 export default withMT({
     content: [
@@ -10,6 +9,8 @@ export default withMT({
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.jsx',
+        './node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}',
+        './node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}',
     ],
 
     theme: {
@@ -17,6 +18,18 @@ export default withMT({
             fontFamily: {
                 sans: ['Poppins', ...defaultTheme.fontFamily.sans],
                 inter: ["Inter", 'sans-serif']
+            },
+            colors: {
+                primary: {
+                    DEFAULT: '#4f46e5', // Custom primary color
+                    dark: '#3730a3',
+                    light: '#a5b4fc',
+                },
+                gray: {
+                    light: '#f9fafb',
+                    DEFAULT: '#6b7280',
+                    dark: '#374151',
+                },
             },
         },
     },
